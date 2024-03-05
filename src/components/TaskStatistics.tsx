0f39bea62b248aa06e1f82757deca03033913c1e
vherasymenko <manqarium@gmail.com>
2024-03-05 14:40:25 +0200

    [Feature] add taskStatistics

---
+++ src/components/TaskStatistics.tsx
@@ -0,0 +1,24 @@
+import { Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import React from 'react'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+
+export const TaskStatistics = () => {
+  const { tasks } = useTypedSelector((state) => state.tasks)
+  const completedTasks = tasks.filter((task) => task.completed).length
+  const uncompletedTasks = tasks.length - completedTasks
+
+  return (
+    <Box display="inline-flex" gap={1}>
+      <Typography variant="body2" color="secondary.main" mb={2}>
+        Completed:{' '}
+        <Typography variant="body2" color="success.main" display="inline-flex">
+          <b>{completedTasks}</b>
+        </Typography>
+      </Typography>
+      <Typography variant="body2" color="secondary.main" mb={2}>
+        | Uncompleted: <b>{uncompletedTasks}</b>
+      </Typography>
+    </Box>
+  )
+}
